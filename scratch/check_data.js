5d7c22e98b155615c34ac065cd98d9d93f3fd086
coconutswallow <coconut.hawthorne@gmail.com>
2026-04-22 06:13:30 -0700

    fix links

--- scratch/check_data.js
+++
@@ -1,9 +0,0 @@
-import { getEquipment } from './assets/js/allowed-content/ac-service.js';
-
-async function checkData() {
-    const data = await getEquipment();
-    const special = data.filter(i => i.name.includes('Barding') || i.name.includes('Adamantine'));
-    console.log(JSON.stringify(special, null, 2));
-}
-
-checkData();
